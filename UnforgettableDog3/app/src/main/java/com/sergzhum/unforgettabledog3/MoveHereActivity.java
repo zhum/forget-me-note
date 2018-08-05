95eacd5645d2d93eb459d89cc7565ec520834cf7
Sergey Zhumatiy <serg@parallel.ru>
2018-08-05 19:21:17 +0300

    Android design added

---
+++ UnforgettableDog3/app/src/main/java/com/sergzhum/unforgettabledog3/MoveHereActivity.java
@@ -0,0 +1,13 @@
+package com.sergzhum.unforgettabledog3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoveHereActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_here);
+    }
+}
